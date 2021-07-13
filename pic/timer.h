36b5f35e183606862fc60b808f083fc539f956ac
krmk22 <58293898+krmk22@users.noreply.github.com>
2021-07-13 17:23:33 +0530

    Create timer.h

---
+++ pic/timer.h
@@ -0,0 +1,10 @@
+#ifndef _TIMER_H
+#define _TIMER_H	
+
+/*************************************************************/
+/*****EXTERN FUNCTION FOR MILLIS & MICROS GENERATION**********/
+/*************************************************************/
+extern void timer_init_all();
+extern unsigned long millis();
+extern unsigned long micros();
+#endif
